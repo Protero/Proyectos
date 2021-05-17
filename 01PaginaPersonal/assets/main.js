3bbbcdddc1bbf26e0c65221763eb5461ba123ca5
Protero <protero@outlook.es>
2021-05-17 19:23:13 +0200

    script:link

--- 01PaginaPersonal/assets/main.js
+++ 01PaginaPersonal/assets/main.js
@@ -1,13 +1,32 @@
+console.log("> cargado main.js");
 
 let search={
     Contacto: "contacto.html",
-    Biografia: "biografia.html"
+    Biografia: "biografia.html",
+    Buscador: "result.html"
 }
 
+let tab1 = document.getElementById("m1");
+let tab2 = document.getElementById("m2");
+let tab3 = document.getElementById("m3");
+tab1.addEventListener("focus",(e)=>{
+    console.log("foco", tab1);
+    document.addEventListener("keydown", (e)=>{
+        console.log(e);
+        tab2.focus();
+        tab1.setAttribute("tabIndex", "-1");
+    })
+},true);
+tab2.addEventListener("focus",(e)=>{
+    console.log("foco", tab2);
+},true);
+tab3.addEventListener("focus",(e)=>{
+    console.log("foco", tab3);
+},true);
 
 
 window.onload = function (){
-  
+    
     console.log(window.location.search);
     let parametros = new URLSearchParams(window.location.search);
     let result = document.getElementById("result"); 
