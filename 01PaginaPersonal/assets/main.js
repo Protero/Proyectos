197e36ac1ee24354782bfadf8e8a516838d87703
Protero <protero@outlook.es>
2021-05-18 12:34:24 +0200

    script menu

--- 01PaginaPersonal/assets/main.js
+++ 01PaginaPersonal/assets/main.js
@@ -1,23 +1,36 @@
 console.log("> cargado main.js");
 
-let search={
-    Contacto: "contacto.html",
-    Biografia: "biografia.html",
-    Buscador: "result.html"
+let keyCode = function (tab){
+    tab.addEventListener("keyup", (e)=>{
+        switch (e.key){
+            case "ArrowRight":
+                console.log("> entro botón flecha derecha");
+                document.getElementById("m2").focus();
+                break;
+            default:
+                console.log("> keyCode defeult",e);    
+        }
+        console.log("> keyCode funtión",e);
+        // tab2.focus();
+        // tab1.setAttribute("tabIndex", "-1");
+    },true)
 }
 
-// let tab1 = document.getElementById("m1");
-// let tab2 = document.getElementById("m2");
-// let tab3 = document.getElementById("m3");
-// tab1.addEventListener("focus",(e)=>{
-//     console.log("foco", tab1);
-//     tab2.focus();
-//     document.addEventListener("keydown", (e)=>{
-//         console.log(e);
-//         tab2.focus();
-//         tab1.setAttribute("tabIndex", "-1");
-//     })
-// },true);
+
+
+let tab1 = document.getElementById("m1");
+let tab2 = document.getElementById("m2");
+let tab3 = document.getElementById("m3");
+tab1.addEventListener("focus",(e)=>{
+    console.log("foco", tab1);
+    keyCode(tab1);
+
+    // document.addEventListener("keydown", (e)=>{
+    //     console.log(e);
+    //     tab2.focus();
+    //     tab1.setAttribute("tabIndex", "-1");
+    // },false)
+},true);
 // tab2.addEventListener("focus",(e)=>{
 //     console.log("foco", tab2);
 // },true);
@@ -25,6 +38,13 @@ let search={
 //     console.log("foco", tab3);
 // },true);
 
+
+
+let search={
+    Contacto: "contacto.html",
+    Biografia: "biografia.html",
+    Buscador: "result.html"
+}
 window.onload = function (){
     
     console.log(window.location.search);
