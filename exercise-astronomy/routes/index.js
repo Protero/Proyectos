1fcd4ae9d027f0a0111a09213a1048018459cf6d
Protero <protero@outlook.es>
2021-07-03 13:02:34 +0200

    added: neas path in index Router

--- exercise-astronomy/routes/index.js
+++ exercise-astronomy/routes/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 
 router.use("/landings",require("../routes/landingsRoutes"));
+router.use("/neas",require("../routes/neasRoutes"));
 
 module.exports = router;
