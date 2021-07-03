d2db7b4591c27751932906ed6d462debb9e1760b
Protero <protero@outlook.es>
2021-07-03 12:59:50 +0200

    create: NeasRouters folder and index file

---
+++ exercise-astronomy/routes/neasRoutes/index.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const NeasModel = require("../../models/NeasSchema");
+
+
+router.get("/get-neas",async(req, res, next)=>{
+    const result = await NeasModel.find({}, { _id: 0, __v: 0 });
+    console.log(">neas: ", result);
+    res.send(result);
+})
+
+module.exports = router;
