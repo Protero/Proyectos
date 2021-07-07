0a873721b8962f6127063b1e21faec654190ed6b
Protero <protero@outlook.es>
2021-07-07 17:43:31 +0200

    Create: constans.js with values

--- exercise-astronomy/index.js
+++ exercise-astronomy/index.js
@@ -1,15 +1,14 @@
+require("./config/db");
 const express = require("express");
 const app = express();
-const port = 3000;
-
-require("./config/db");
+const {PORT} = require("./constants");
 
 
 app.use(express.json());
 
 app.use("/astronomy",require("./routes"));
 
-app.listen(port,()=>{
-    console.log(`> Open server http://localhost:${port}`);
+app.listen(PORT,()=>{
+    console.log(`> Open server http://localhost:${PORT}`);
 })
 
