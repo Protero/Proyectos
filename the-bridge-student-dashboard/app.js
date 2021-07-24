a9dce049be3f7673dd018432f42a77581508f010
Protero <protero@outlook.es>
2021-07-24 19:14:24 +0200

    Added: require of routes, auth and bodyparse

--- the-bridge-student-dashboard/app.js
+++ the-bridge-student-dashboard/app.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
-var debug = require('debug')('app');
+const debug = require('debug')('app');
+const passport = require('passport');
+const bodyParser = require("body-parser");
 
 const {PORT} = require('./constants');
+const routes = require('./routes');
 
-require('./db');
+require('./db/index');
+require('./auth/index');
 
 const app = express();
+
+// app.use(passport.initialize());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/", routes);
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({ error: err });
+  });
 
 app.listen(PORT, () => {
     debug(`Server starte http://localhost:${PORT}`); 
