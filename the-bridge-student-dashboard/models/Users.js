884bb0f588d444db10bca2855ffdf8e6b172ce8f
Protero <protero@outlook.es>
2021-07-25 15:54:34 +0200

    Added: pre method with save

--- the-bridge-student-dashboard/models/Users.js
+++ the-bridge-student-dashboard/models/Users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
 
 const UsersSchema = new mongoose.Schema({
     email: {
@@ -10,6 +11,12 @@ const UsersSchema = new mongoose.Schema({
         type: String,
     },
 
+});
+
+UsersSchema.pre("save", async function (next) {
+    const hash = await bcrypt.hash(this.password,10);
+    this.password = hash;
+    next();
 });
 
 const UserModel = mongoose.model("Users", UsersSchema);
