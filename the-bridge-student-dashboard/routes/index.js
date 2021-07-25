025ddb6eae04032e309a1af0f9c3b344262fb051
Protero <protero@outlook.es>
2021-07-25 18:32:05 +0200

    Added: (Routes) login router

--- the-bridge-student-dashboard/routes/index.js
+++ the-bridge-student-dashboard/routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-
+const debug = require('debug')('app');
 
 router.get('/', (req,res,next)=>{
     res.send('Bienvenidos!!');
@@ -10,12 +10,54 @@ router.get('/', (req,res,next)=>{
 router.post(
     '/signup',
     passport.authenticate('signup', {session:false}),
-        async(req,res,next)=>{
-            res.json({
+    async(req,res,next)=>{
+        res.json(
+            {
                 message: 'Signup succesful',
                 user: req.user,
-            });
-        }
-    );
+            }
+        );
+    }
+);
+router.post(
+    '/login', 
+    passport.authenticate('login', {session:false}),
+    async function(req, res, next) {
+        res.json(
+            {
+                message: 'login succesful',
+                user: req.user,
+            }
+        );
+    }
+);
+
+// router.post(
+//     "/login",
+//     async (req, res, next) => {
+//       passport.authenticate("login", async (err, user, info) => {
+//          console.log("> user/login: ", user);
+//         try {
+//           if (err || !user) {
+//             const error = new Error("An error occurred.");
+          
+//             return next(error);
+//           }
+//           req.login(user, { session: false }, async (error) => {
+//             if (error) return next(error);
+          
+//             const body = { _id: user._id, email: user.email };
+//             // const token = jwt.sign({ user: body }, process.env.TOKEN_SECRET);
+          
+//             // return res.json({ token });
+//             return res.json({ body });
+//           });
+//         } catch (error) {
+//           return next(error);
+//         }
+//       })(req, res, next);
+//     });
+
+
 
 module.exports = router;
