c8be606151e8a0df6f51c80710720d98c2825be6
Protero <protero@outlook.es>
2021-07-26 22:41:33 +0200

    modified: (Routes) logind and added token

--- the-bridge-student-dashboard/routes/index.js
+++ the-bridge-student-dashboard/routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const debug = require('debug')('app');
+const jwt = require("jsonwebtoken");
 
 router.get('/', (req,res,next)=>{
     res.send('Bienvenidos!!');
@@ -19,44 +20,61 @@ router.post(
         );
     }
 );
-router.post(
-    '/login', 
-    passport.authenticate('login', {session:false}),
-    async function(req, res, next) {
-        res.json(
-            {
-                message: 'login succesful',
-                user: req.user,
-            }
-        );
-    }
-);
-
 // router.post(
-//     "/login",
-//     async (req, res, next) => {
-//       passport.authenticate("login", async (err, user, info) => {
-//          console.log("> user/login: ", user);
-//         try {
-//           if (err || !user) {
-//             const error = new Error("An error occurred.");
+//     '/signup',
+//     async (req,res,next)=>{
+//         passport.authenticate('signup', async(err,user,info)=>{
+            
+//             res.json(
+//                 {
+//                     message: 'Signup succesful',
+//                     user: user,
+//                 }
+//             );
+//         })
+//     }
+  
+
+// );
+// router.post(
+//     '/login', 
+//     passport.authenticate('login', {session:false}),
+//     async function(req, res, next) {
+//              res.json(
+//                 {
+//                     message: 'login succesful',
+//                     user: req.user,
+//                 }
+//             );
+      
+     
+//     }
+// );
+
+router.post(
+    "/login",
+    async (req, res, next) => {
+      passport.authenticate("login", async (err, user, info) => {
+        try {
+          if (err || !user) {
+            const error = new Error("An error occurred.");
           
-//             return next(error);
-//           }
-//           req.login(user, { session: false }, async (error) => {
-//             if (error) return next(error);
+            return next(error);
+          }
+          req.login(user, { session: false }, async (error) => {
+            if (error) return next(error);
           
-//             const body = { _id: user._id, email: user.email };
-//             // const token = jwt.sign({ user: body }, process.env.TOKEN_SECRET);
+            const body = { _id: user._id, email: user.email };
+            const token = jwt.sign({ user: body }, 'sdkfoj');
           
-//             // return res.json({ token });
-//             return res.json({ body });
-//           });
-//         } catch (error) {
-//           return next(error);
-//         }
-//       })(req, res, next);
-//     });
+            return res.json({ token });
+ 
+          });
+        } catch (error) {
+          return next(error);
+        }
+      })(req, res, next);
+    });
 
 
 
